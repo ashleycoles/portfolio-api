a17146018589818ad3d70f67eba134c6ef598d26
Ashley Coles <ashley@acoles.co.uk>
2025-02-26 15:38:41 +0000

    Basic post model and db

---
+++ app/Models/Post.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Database\Factories\PostFactory;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    /** @use HasFactory<PostFactory> */
+    use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'content',
+        'featuredImage'
+    ];
+}
