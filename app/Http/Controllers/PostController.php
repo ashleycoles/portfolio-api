78a16096f0c28473676c3ca6e03118380698c9a3
Ashley Coles <ashley@acoles.co.uk>
2025-02-26 15:52:02 +0000

    Caching all posts

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -5,12 +5,17 @@ namespace App\Http\Controllers;
 use App\Models\Post;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Log;
 
 class PostController extends Controller
 {
     public function all(): JsonResponse
     {
-        $posts = Post::select(['id', 'title', 'featuredImage'])->get();
+        $posts = Cache::rememberForever('posts', function () {
+            Log::info('Caching posts');
+            return Post::select(['id', 'title', 'featuredImage'])->get();
+        });
 
         return response()->json([
             'data' => $posts,
