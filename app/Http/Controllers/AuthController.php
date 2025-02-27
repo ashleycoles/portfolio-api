fde90c5a3a8115ccc7000d4fe55b95bbd6478849
Ashley Coles <ashley@acoles.co.uk>
2025-02-27 16:33:35 +0000

    Basic auth with sanctum

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\LoginRequest;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    public function login(LoginRequest $request): JsonResponse
+    {
+        if (!Auth::attempt($request->validated())) {
+            return response()->json([
+                'message' => 'Invalid credentials'
+            ], 401);
+        }
+
+        $user = Auth::user();
+
+        $token = $user->createToken('auth-token')->plainTextToken;
+
+        return response()->json([
+            'token' => $token,
+        ]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,17 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
 Route::controller(PostController::class)->group(function () {
     Route::get('/posts', 'all')->name('posts.all');
-    Route::post('/posts', 'store')->name('posts.store');
+
+    Route::post('/posts', 'store')
+        ->middleware('auth:sanctum')
+        ->name('posts.store');
+});
+
+Route::controller(AuthController::class)->group(function () {
+    Route::post('/login', 'login')->name('auth.login');
 });
